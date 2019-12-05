52ee7b48827683b917842786b85d63d5f43f639c
hoidal <sjhoidal@gmail.com>
2019-12-05 15:43:07 -0700

    Added gitignore file

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_221204) do
-
-  create_table "games", force: :cascade do |t|
-    t.string "status"
-    t.integer "user_id"
-    t.string "computer_call"
-    t.string "call"
-    t.index ["user_id"], name: "index_games_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
